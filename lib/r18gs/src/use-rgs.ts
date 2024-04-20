b4782b4f10197e3fe3032a8c19e251cb3cba9e75
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-04-20 15:03:45 +0530

    Add support for extensions.

--- lib/r18gs/src/use-rgs.ts
+++ lib/r18gs/src/use-rgs.ts
@@ -3,14 +3,20 @@ import { useSyncExternalStore } from "react";
 
 type Listener = () => void;
 type Subscriber = (l: Listener) => () => void;
+type Mutate<T> = (value?: T, serverValue?: T) => void;
+
 export type SetterArgType<T> = T | ((prevState: T) => T);
 export type SetStateAction<T> = (value: SetterArgType<T>) => void;
+export type Extension<T> = {
+	init?: (key: string, value: T | undefined, serverValue: T | undefined, mutate: Mutate<T>) => void;
+	onChange?: (key: string, value?: T, serverValue?: T) => void;
+};
 
 /**
  * This is a hack to reduce lib size + readability + not encouraging direct access to globalThis
  */
-const [VALUE, LISTENERS, SETTER, SUBSCRIBER] = [0, 1, 2, 3];
-type RGS = [unknown, Listener[], SetStateAction<unknown>, Subscriber];
+const [VALUE, SERVER_VALUE, LISTENERS, SETTER, SUBSCRIBER] = [0, 1, 2, 3, 4];
+type RGS = [unknown, unknown, Listener[], SetStateAction<unknown>, Subscriber];
 
 declare global {
 	// eslint-disable-next-line no-var -- var required for global declaration.
@@ -21,14 +27,36 @@ const globalThisForBetterMinification = globalThis;
 globalThisForBetterMinification.rgs = {};
 const globalRGS = globalThisForBetterMinification.rgs;
 
+function triggerListeners(rgs: RGS) {
+	(rgs[LISTENERS] as Listener[]).forEach(listener => listener());
+}
+
+let allExtentionsInitialized = false;
+/** Initialize extestions - wait for previous extension's promise to be resolved before processing next */
+async function initExtentions<T>(key: string, extensions: Extension<T>[]) {
+	const rgs = globalRGS[key] as RGS;
+	const mutate: Mutate<T> = (newValue, newServerValue) => {
+		[rgs[VALUE], rgs[SERVER_VALUE]] = [newValue, newServerValue];
+	};
+	for (const ext of extensions) {
+		/** Next extension initializer will get the new value if updated by previous one */
+		await ext.init?.(key, rgs[VALUE] as T, rgs[SERVER_VALUE] as T, mutate);
+	}
+	triggerListeners(rgs);
+	allExtentionsInitialized = true;
+}
+
 /** Initialize the named store when invoked for the first time. */
-function init<T>(key: string, value?: T) {
+function init<T>(key: string, value?: T, serverValue?: T, extensions: Extension<T>[] = []) {
 	const listeners: Listener[] = [];
 	/** setter function to set the state. */
 	const setter: SetStateAction<T> = val => {
+		/** Do not allow mutating the store before all extentions are initialized */
+		if (!allExtentionsInitialized) return;
 		const rgs = globalRGS[key] as RGS;
 		rgs[VALUE] = val instanceof Function ? val(rgs[VALUE] as T) : val;
-		(rgs[LISTENERS] as Listener[]).forEach(listener => listener());
+		triggerListeners(rgs);
+		extensions.forEach(ext => ext.onChange?.(key, rgs[VALUE] as T, rgs[SERVER_VALUE] as T));
 	};
 	/** subscriber function to subscribe to the store. */
 	const subscriber: Subscriber = listener => {
@@ -39,7 +67,8 @@ function init<T>(key: string, value?: T) {
 			rgs[LISTENERS] = listeners.filter(l => l !== listener);
 		};
 	};
-	globalRGS[key] = [value, listeners, setter as SetStateAction<unknown>, subscriber];
+	globalRGS[key] = [value, serverValue, listeners, setter as SetStateAction<unknown>, subscriber];
+	initExtentions(key, extensions);
 }
 
 /**
@@ -58,17 +87,22 @@ function init<T>(key: string, value?: T) {
  * @param serverValue - Server value of the store.
  * @returns - A tuple (Ordered sequance of values) containing the state and a function to set the state.
  */
-export default function useRGS<T>(key: string, value?: T, serverValue?: T): [T, SetStateAction<T>] {
-	if (!globalRGS[key]) init(key, value);
+export default function useRGS<T>(
+	key: string,
+	value?: T,
+	serverValue?: T,
+	extensions?: Extension<T>[],
+): [T, SetStateAction<T>] {
+	if (!globalRGS[key]) init(key, value, serverValue, extensions);
 
 	const rgs = globalRGS[key] as RGS;
 
 	/** Function to set the state. */
 	const setRGState = rgs[SETTER] as SetStateAction<T>;
 	/** Function to get snapshot of the state. */
-	const getSnap = () => (rgs[VALUE] ?? value) as T;
+	const getSnap = () => rgs[VALUE] as T;
 	/** Function to get server snapshot. Returns server value is provided else the default value. */
-	const getServerSnap = () => (serverValue ?? rgs[VALUE] ?? value) as T;
+	const getServerSnap = () => (rgs[SERVER_VALUE] ?? rgs[VALUE]) as T;
 
 	const val = useSyncExternalStore<T>(rgs[SUBSCRIBER] as Subscriber, getSnap, getServerSnap);
 	return [val, setRGState];
