cdd923c39e56bc6189f982a8162eca8dd1568ba8
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-04-21 08:48:51 +0530

    Clean up a bit more

--- lib/r18gs/tsup.config.ts
+++ lib/r18gs/tsup.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "tsup";
+import fs from "node:fs";
+import path from "node:path";
 
 export default defineConfig(options => ({
 	format: ["cjs", "esm"],
@@ -8,4 +10,37 @@ export default defineConfig(options => ({
 	bundle: true,
 	minify: !options.watch,
 	legacyOutput: true,
+	esbuildPlugins: [
+		{
+			name: "improve-minify",
+			setup(build) {
+				build.onLoad({ filter: /use-rgs.ts/ }, args => {
+					let contents = fs.readFileSync(args.path, "utf8");
+					const lines = contents.split("\n");
+					const hackLine = lines.find(line => line.startsWith("const [VALUE,"));
+
+					if (!hackLine) return { contents, loader: "ts" };
+					/** remove hackLine */
+					contents = contents.replace(hackLine, "");
+
+					/** clean up */
+					const tokens = hackLine
+						.replace("const", "")
+						.split("=")[0]
+						.trim()
+						.replace(/\]|\[/g, "")
+						.split(",")
+						.map(t => t.trim());
+
+					tokens.sort((a, b) => a.length - b.length);
+
+					for (let i = 0; i < tokens.length; i++) {
+						contents = contents.replace(new RegExp(`${tokens[i]}`, "g"), i + "");
+					}
+					console.log({ contents });
+					return { contents, loader: "ts" };
+				});
+			},
+		},
+	],
 }));
