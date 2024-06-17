ca38dcab90ed288fe1d9cdd7df0156ba80b234bf
Mayank <mayank.srmu@gmail.com>
2024-06-17 19:07:59 +0530

    Add coments

--- packages/shared/src/client/demo/counter.tsx
+++ packages/shared/src/client/demo/counter.tsx
@@ -2,11 +2,13 @@ import useRGS from "r18gs";
 
 const RGS_COUNTER_KEY = "counter";
 
+/** Counter Controller */
 export const CounterController = () => {
   const [counter, setCounter] = useRGS(RGS_COUNTER_KEY, 0);
   return <input type="number" value={counter} onChange={e => setCounter(Number(e.target.value))} />;
 };
 
+/** Counter Display  */
 export const CounterDisplay = () => {
   const [counter] = useRGS(RGS_COUNTER_KEY, 0);
   return <div>{counter}</div>;
