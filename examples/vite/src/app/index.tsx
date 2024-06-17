40c3c41c1c778bb29c5a25b07594f46768ff3b9e
Mayank <mayank.srmu@gmail.com>
2024-06-17 19:06:01 +0530

    Update vite example

--- examples/vite/src/app/index.tsx
+++ examples/vite/src/app/index.tsx
@@ -1,15 +1,13 @@
-import React, { useCallback } from "react";
+import React from "react";
 import "./styles.css";
-import { Bars1, Bars2, Dots1, Dots2 } from "react18-loaders/dist/server";
-import { LoaderContainer, useLoader } from "react18-loaders";
+import { Bars1 } from "react18-loaders/dist/server";
+import { LoaderContainer } from "react18-loaders";
 import { LandingPage, Layout } from "@repo/shared/dist/server";
 import { Core } from "nextjs-darkmode";
 import { Demo, Header } from "@repo/shared";
 
 /** Vite App */
 function App(): JSX.Element {
-  const { setLoading } = useLoader();
-  const handleClick = useCallback(() => setLoading(true), []);
   return (
     <Layout>
       <Core t="background .5s" />
@@ -17,7 +15,6 @@ function App(): JSX.Element {
       <LandingPage title="Vite Example">
         <Demo />
       </LandingPage>
-      <button onClick={handleClick}>Show loader</button>
       <LoaderContainer>
         <Bars1 color="red" width={50} />
       </LoaderContainer>
