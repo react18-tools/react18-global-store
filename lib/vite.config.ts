3150924926673bc08a6a699517b469c29729862e
Mayank <mayank.srmu@gmail.com>
2024-07-12 19:03:08 +0530

    Use vite to build library

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { globSync } from "glob";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: globSync("src/**/*.ts", { ignore: "**/*.test.*" }),
+    },
+    rollupOptions: {
+      output: [
+        { preserveModules: true, format: "cjs" },
+        { preserveModules: true, format: "es" },
+      ],
+      external: ["react", "react-dom"],
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
