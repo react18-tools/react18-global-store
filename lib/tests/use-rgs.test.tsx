bd9020b526e2d373d5d847bc8dc3ffde34c7adef
Mayank <mayank.srmu@gmail.com>
2024-12-16 09:26:53 +0530

    imporve test branch coverage

--- lib/tests/use-rgs.test.tsx
+++ lib/tests/use-rgs.test.tsx
@@ -1,7 +1,8 @@
 import { describe, test } from "vitest";
-import { act, fireEvent, render, screen } from "@testing-library/react";
+import { act, cleanup, fireEvent, render, screen } from "@testing-library/react";
 import { useRGS } from "../src";
 import { ChangeEvent, useCallback } from "react";
+import { beforeEach } from "node:test";
 
 const COUNT_RGS_KEY = "count";
 const TESTID_INPUT = "in1";
@@ -27,11 +28,24 @@ function Component2() {
   return <h1 data-testid={TESTID_DISPLAY}>{count}</h1>;
 }
 
+function Component3() {
+  const [count] = useRGS<number>(COUNT_RGS_KEY + 1, () => 5);
+  return <div data-testid="t3">{count}</div>;
+}
+
 describe("React18GlobalStore", () => {
+  beforeEach(() => {
+    cleanup();
+    // reset RGS store
+    globalThis.rgs = {};
+  });
   test("check state update to multiple components", async ({ expect }) => {
     render(<Component1 />);
     render(<Component2 />);
+    render(<Component3 />);
     await act(() => fireEvent.input(screen.getByTestId(TESTID_INPUT), { target: { value: 5 } }));
     expect(screen.getByTestId(TESTID_DISPLAY).textContent).toBe("5");
+    // test function initialization
+    expect(screen.getByTestId("t3").textContent).toBe("5");
   });
 });
