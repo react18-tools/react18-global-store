5d6fe58fdc09b9636d2b15dc86748b5a6aefe936
Mayank <mayank.srmu@gmail.com>
2024-12-17 12:36:51 +0530

    rebrand examples

--- packages/shared/src/client/demo/demo.tsx
+++ packages/shared/src/client/demo/demo.tsx
@@ -1,59 +1,28 @@
 "use client";
 
-import { CounterController, CounterDisplay } from "./counter";
+import { BasicExample } from "./basic-example";
 import styles from "./demo.module.scss";
-import { Editor } from "react-live";
+import basicExampleCode from "./basic-example.tsx?raw";
+import counterCode from "./counter.tsx?raw";
+import { CodeDisplay } from "./code-display";
 
-const code = `import { useRGS } from "r18gs";
-
-const MY_KEY = "counter";
-
-export const CounterController = () => {
-  const [counter, setCounter] = useRGS(MY_KEY, 0);
-  return (
-    <input
-      type="number"
-      value={counter}
-      onChange={e =>
-        setCounter(Number(e.target.value))
-      }
-    />
-  );
-};
-
-export const CounterDisplay = () => {
-  const [counter] = useRGS(MY_KEY, 0);
-  return <div>{counter}</div>;
-};`;
+const code = [
+  { filename: "basic-example.tsx", code: basicExampleCode },
+  { filename: "counter.tsx", code: counterCode },
+];
 
 /** React live demo */
 export function Demo() {
   return (
-    <div className={styles.demo}>
-      <Editor className={styles.code} code={code} language="tsx" />
-      <div className={styles.preview}>
-        <p>All components below share the same state without any wrapper.</p>
-        <p>
-          <img src="https://img.shields.io/bundlephobia/minzip/r18gs" alt="NPM Bundle Size" />
-        </p>
-        <div className={styles.center}>
-          <h2>Counter Controller 1</h2>
-          <CounterController />
-        </div>
-        <div className={styles.center}>
-          <h2>Counter Display 1</h2>
-          <CounterDisplay />
-        </div>
-        <br />
-        <div className={styles.center}>
-          <h2>Counter Controller 2</h2>
-          <CounterController />
-        </div>
-        <div className={styles.center}>
-          <h2>Counter Display 2</h2>
-          <CounterDisplay />
-        </div>
+    <>
+      <div className={styles.demo}>
+        <h2 className={styles.preview}>A tiny yet powerful store for modern react libraries.</h2>
+        <img src="https://img.shields.io/bundlephobia/minzip/r18gs" alt="NPM Bundle Size" />
       </div>
-    </div>
+      <div className={styles.demo}>
+        <BasicExample />
+        <CodeDisplay code={code} />
+      </div>
+    </>
   );
 }
