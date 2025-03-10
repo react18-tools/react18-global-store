2ec9320c28c5ef8b1346f31709af3136ca71806f
Mayank <mayank.srmu@gmail.com>
2025-03-10 12:08:42 +0530

    Do not mark deprecated

--- scripts/publish-canonical.js
+++ scripts/publish-canonical.js
@@ -12,8 +12,3 @@ const { execSync } = require("child_process");
   execSync(`sed -i -e "s/name.*/name\\": \\"${pkg}\\",/" lib/package.json`);
   execSync("cd lib && npm publish --provenance --access public");
 });
-
-// Mark deprecated
-execSync(
-  'npm deprecate @mayank1513/r18gs "Please use https://www.npmjs.com/package/r18gs instead. We initially created scoped packages to have similarities with the GitHub Public Repository (which requires packages to be scoped). We are no longer using GPR and thus deprecating all scoped packages for which corresponding un-scoped packages exist."',
-);
